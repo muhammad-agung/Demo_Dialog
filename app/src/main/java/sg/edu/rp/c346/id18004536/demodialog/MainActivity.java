8705213f8ea546606cbb774bfc9c0da87d7436e6
MUHAMMAD AGUNG RAMADHAN RP <18004536@myrp.edu.sg>
2020-08-03 10:07:07 +0800

    added new faature demo2

--- app/src/main/java/sg/edu/rp/c346/id18004536/demodialog/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id18004536/demodialog/MainActivity.java
@@ -1,15 +1,21 @@
 package sg.edu.rp.c346.id18004536.demodialog;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnDemo1;
+    Button btnDemo2;
+    TextView tvDemo2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnDemo1 = findViewById(R.id.buttonDemo1);
+        btnDemo2 = findViewById(R.id.buttonDemo2);
+        tvDemo2 = findViewById(R.id.TextViewDemo2);
 
         btnDemo1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,11 +32,48 @@ public class MainActivity extends AppCompatActivity {
 
                 AlertDialog.Builder myBuilder = new AlertDialog.Builder(MainActivity.this);
 
-                myBuilder.setTitle("Demo 1- Simple dialog");
-                myBuilder.setMessage("I can develop Andorid app");
-                myBuilder.setCancelable(false);
-                myBuilder.setPositiveButton("Close", null);
+//                myBuilder.setTitle("Demo 1- Simple dialog");
+//                myBuilder.setMessage("I can develop Andorid app");
+//                myBuilder.setCancelable(false);
+//                myBuilder.setPositiveButton("Close", null);
 
+                myBuilder.setTitle("Congratulations");
+                myBuilder.setMessage("You have completed a single Dialog Box");
+                myBuilder.setCancelable(false);
+                myBuilder.setPositiveButton("Dismiss", null);
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+            }
+        });
+
+
+        btnDemo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                myBuilder.setTitle("Demon 2 Button Dialog");
+                myBuilder.setMessage("Select one of the button below");
+                myBuilder.setCancelable(false);
+
+                myBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tvDemo2.setText("You have selected positive");
+                    }
+                });
+
+
+                myBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tvDemo2.setText("You have selected negative");
+                    }
+                });
+
+                myBuilder.setNeutralButton("Cancel", null);
                 AlertDialog myDialog = myBuilder.create();
                 myDialog.show();
             }
